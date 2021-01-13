e2103494908ff65c0f68912a1f7627e6120fae78
Abelrabbit19961216 <>
2021-01-13 21:19:29 +0800

    2020.01.13 更新2

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ $("#banner").slick({
    autoplaySpeed:2000,
     dots:true
 });
+$("#service").slick({
+    slidesToShow: 3,
+    slidesToScroll:2,
+    autoplay:true,
+    autoplaySpeed:2000,
+    dots:true
+  });
